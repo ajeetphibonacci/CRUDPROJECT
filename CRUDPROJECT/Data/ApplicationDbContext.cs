7fc502c44874a70967d19a13607b781da5ad6027
Ajeet <ajeetpandey551@gmail.com>
2022-05-30 11:32:41 +0530

    Crud

---
+++ CRUDPROJECT/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CRUDPROJECT.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDPROJECT.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)        
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+        
+
+    }
+}
--- CRUDPROJECT/Program.cs
+++ CRUDPROJECT/Program.cs
@@ -1,8 +1,12 @@
+using CRUDPROJECT.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DbCon")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
